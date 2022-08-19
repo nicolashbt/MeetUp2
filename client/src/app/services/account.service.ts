7125cf997f7146cf47f85411ffb9d3611c4d966e
Nicolas Hbt <nicolas.hbt@pm.me>
2022-08-19 13:34:19 +0200

    add register

--- client/src/app/services/account.service.ts
+++ client/src/app/services/account.service.ts
@@ -15,8 +15,18 @@ export class AccountService {
 
   login(model: any) {
     return this.http.post(this.baseUrl + "account/login/", model).pipe(
-      map((reponse: User) => {
-        const user = reponse;
+      map((user: User) => {
+        if (user) {
+          localStorage.setItem("user", JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
+
+  register(model: any) {
+    return this.http.post(this.baseUrl + "account/register/", model).pipe(
+      map((user: User) => {
         if (user) {
           localStorage.setItem("user", JSON.stringify(user));
           this.currentUserSource.next(user);
